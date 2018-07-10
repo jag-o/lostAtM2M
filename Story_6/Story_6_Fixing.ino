bec29aa14e975d59c75e6d1daca4ab15f14792e8
Jago <jagogardiner@gmail.com>
2018-07-10 13:27:51 +0100

    fixing story 6

--- Story_6/Story_6_Fixing.ino
+++ Story_6/Story_6_Fixing.ino
@@ -1,3 +1,4 @@
+// Initialize variables for pins and speed.
 const int left_feedback_pin = 2;
 const int right_feedback_pin = 3;
 const int EnableLeft = 9;
@@ -8,16 +9,17 @@ const int ForwardsRight = 12;
 const int BackwardsRight = 11;
 const int LeftSpeed = 130;
 const int RightSpeed = 255;
-
+// These counters are used for the amount of times the feedback from the motors come through.
 volatile unsigned int left_count = 0;
 volatile unsigned int right_count = 0;
-
+// These methods are used for increasing the counters if the interrupt is hit.
 void left_pulse_interrupt() {
     left_count++;
 }
 void right_pulse_interrupt() {
     right_count++;
 }
+// checkCounter() is a method to readjust the motors speed to make the counters the same (hopefully).
 void checkCounter() {
     if(left_count - right_count < 0) {
         RightSpeed = (RightSpeed - 20);
@@ -26,6 +28,17 @@ void checkCounter() {
         LeftSpeed = (LeftSpeed - 20);
     }
 }
+void moveTheRover() {
+    // Move the GODDAMN ROVER
+    analogWrite(ForwardsLeft, LeftSpeed);
+    analogWrite(ForwardsRight, RightSpeed);
+    digitalWrite(EnableLeft, HIGH);
+    digitalWrite(EnableRight, HIGH);
+    delay(500);
+    checkCounter();
+    delay(500);
+}
+// setup() initializes the pins needed, and also attaches an interrupt.
 void setup() {
     pinMode(left_feedback_pin, INPUT_PULLUP);
     pinMode(right_feedback_pin, INPUT_PULLUP);
@@ -39,7 +52,7 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(right_feedback_pin), right_pulse_interrupt, RISING);
     Serial.begin(115200);
 }
+// loop() is where we'll be using the above code to check these counters and make sure it progresses in a line.
 void loop() {
-    checkCounter();
-    delay(250);
+    moveTheRover();
 }
